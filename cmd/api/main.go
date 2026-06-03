bf436933daab4365a23a9008274ccba5fb1297c6
Livia Indriana Sari <liviaindrisari@gmail.com>
2026-06-03 11:50:27 +0700

    Add Logs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
 )
 
@@ -20,31 +21,33 @@ func main() {
 	db := config.NewDatabase()
 
 	// migrate tabel user
-if err := db.AutoMigrate(&models.User{}); err != nil {
-	log.Fatal("❌ Gagal migrate tabel User:", err)
-}
-log.Println("✅ Berhasil migrate tabel User")
+	if err := db.AutoMigrate(&models.User{}); err != nil {
+		log.Fatal("❌ Gagal migrate tabel User:", err)
+	}
+	log.Println("✅ Berhasil migrate tabel User")
 
-// migrate tabel peminjaman
-if err := db.AutoMigrate(&models.Peminjaman{}); err != nil {
-	log.Fatal("❌ Gagal migrate tabel Peminjaman:", err)
-}
-log.Println("✅ Berhasil migrate tabel Peminjaman")
+	// migrate tabel peminjaman
+	if err := db.AutoMigrate(&models.Peminjaman{}); err != nil {
+		log.Fatal("❌ Gagal migrate tabel Peminjaman:", err)
+	}
+	log.Println("✅ Berhasil migrate tabel Peminjaman")
 
-// migrate tabel ruangan
-if err := db.AutoMigrate(&models.Ruangan{}); err != nil {
-	log.Fatal("❌ Gagal migrate tabel Ruangan:", err)
-}
-log.Println("✅ Berhasil migrate tabel Ruangan")
+	// migrate tabel ruangan
+	if err := db.AutoMigrate(&models.Ruangan{}); err != nil {
+		log.Fatal("❌ Gagal migrate tabel Ruangan:", err)
+	}
+	log.Println("✅ Berhasil migrate tabel Ruangan")
 
-// migrate tabel jadwal
-if err := db.AutoMigrate(&models.Jadwal{}); err != nil {
-	log.Fatal("❌ Gagal migrate tabel Jadwal:", err)
-}
-log.Println("✅ Berhasil migrate tabel Jadwal")
+	// migrate tabel jadwal
+	if err := db.AutoMigrate(&models.Jadwal{}); err != nil {
+		log.Fatal("❌ Gagal migrate tabel Jadwal:", err)
+	}
+	log.Println("✅ Berhasil migrate tabel Jadwal")
 
 	// init app
 	app := fiber.New()
+
+	app.Use(logger.New())
 
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "http://localhost:3000",
